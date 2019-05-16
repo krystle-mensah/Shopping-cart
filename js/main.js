baacba40a5426af8c04c95fd45fcd241b5c569e3
krystle mensah <krystle.webdev@gmail.com>
2019-05-16 10:49:23 +0100

    file delete+new file+script file change position.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Remove Item from cart
+var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+// check variable
+console.log(removeCartItemButtons);
+
+// We need to create a loop to loop through items. 
+for (var i = 0; i < removeCartItemButtons.length; i++){
+  var button = removeCartItemButtons[i]
+  button.addEventListener('click', function() {
+
+  })
+
+}
+
+ 
