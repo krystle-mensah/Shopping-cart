10d373f3c68fef6148fd7376cddafdba2dc9f50e
krystle mensah <krystle.webdev@gmail.com>
2019-05-29 10:48:45 +0100

    code update

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function ready() {
     // So when it is clicked the event runs and the function addToCartClicked.
    
     button.addEventListener('click', addToCartClicked)
-    button.addEventListener('click', saveCart) // console.log(saveCart);   
+    //button.addEventListener('click', saveCart) // console.log(saveCart);   
   }
 
   document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
@@ -179,21 +179,127 @@ function updateCartTotal() {
   document.getElementsByClassName('cart-total-price')[0].innerText = '£' + total 
 }
 
+//SAVING TO LOCAL STORAGE NOTES
 
-function saveCart(event) {
-  // JSON stands for javascript object notation.
-  
-  var shoppingCart = [];
+// Create a cart object and save to variable
+const shoppingCart = {
+  key: 'javascript shopping cart',
+  contents: [],
+  init() {
+    // check local storage and initialize the contents of shoppingCart.contents
+    let _contents = localStorage.getItem(shoppingCart.key);
+    if(_contents){
+      shoppingCart.contents = JSON.parse(_contents);
+    }else{
+      // Dummy test data
+      shoppingCart.contents = [
+        {id: 1, title: 'Bbq crunch', qty: 50, itemPrice: 1.99},
+        {id: 2, title: 'punchy protein', qty: 50, itemPrice: 1.50},
+        {id: 3, title: 'dark Chocolate Cherry', qty: 50, itemPrice: 0.99}
+      ];
+      shoppingCart.sync();
+    }
+  },
+  async sync(){
+    let _cart = JSON.stringify(shoppingCart.contents);
+    await localStorage.setItem(shoppingCart.key, _cart); 
+  },
+  find(id){
+    // Find an item in the cart by its id
+    let match = shoppingCart.contents.filter(item=>{
+      if(item.id == id)
+      return true;
+    });
+    if(match && match[0])
+      return match[0];
+  },
+  add(id){
+    // Add a new item to cart
+    // check that it is not in the cart already
+    if(shoppingCart.find(id)){
+      shoppingCart.increase(id, 1);
+    } else {
+      let arr = PRODUCTS.filter(products=>{
+        if(products.id == id){
+          return true;
 
-  localStorage.setItem('items', JSON.stringify(shoppingCart)); 
-  var data = JSON.parse(localStorage.setItem('items'))
-  
-}
+        }
+      });
+      if(arr && arr[0]){
+        let obj = {
+          id: arr[0].id,
+          title: arr[0].title,
+          qty: 1,
+          itemPrice: arr[0].price
+        };
+        shoppingCart.contents.push(obj);
+        // Update localstorage
+        shoppingCart.sync();
+      } else {
+        // product id does not exist in product data
+        console.error('Invalid Product');
+      }
+    }
+  },
+  increase(id, qty=1){
+    //Increase the quantity of an item in the cart
+    shoppingCart.contents = shoppingCart.contents.map(item =>{
+      if(item.id === id){
+        item.qty = item.qty + qty;
+        return item;
+      }
+    });
+    // update local storage
+    shoppingCart.sync()
+  },
+  reduce(id, qty=1) {
+    //reduce the quantity of an item in the cart
+    shoppingCart.contents = shoppingCart.contents.map(item => {
+      if(item.id === id && item.qty === 0){
+        item.qty = item.qty - qty;
+        return item;
+      }
+    });
+    shoppingCart.contents.forEach(async item => {
+      if(item.id === id && item.qty === 0){
+        shoppingCart.remove(id);
+      }
+    });
+    shoppingCart.sync()
+  },
+  remove(id){
+    // remove an item entirely from the shoppingCart.contents based on it id
+    shoppingCart.contents = shoppingCart.contents.filter(item => {
+      if(item.id !== id){
+        return true;
+      }
+    });
+    //update localstorage
+    shoppingCart.sync() 
+  },
+  empty(){
+    // Empty whole cart
+    shoppingCart.contents = [];
+    // update localstorage
+    shoppingCart.sync()
+  },
+  sort(field='title'){
+    //sort by field - title, price
+    //return a sorted shallow copy of the CART.contents array
+    let sorted = shoppingCart.contents.sort( (a, b)=>{
+        if(a[field] > b[field]){
+            return 1;
+        }else if(a[field] < a[field]){
+            return -1;
+        }else{
+            return 0;
+        }
+    });
+    return sorted;
+    //NO impact on localStorage
+  },
+  logContents(prefix){  
+    console.log(prefix, shoppingCart.contents)
+  }
+};
 
-//localStorage.setItem('user', 'krystle');
-//localStorage.clear();
-//localStorage.removeItem('key');
-
-
-
- 
